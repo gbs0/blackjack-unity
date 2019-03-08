ac3d71246185035272b08cda9d134181e989b8ca
gabriel schiavo <gabriel@lewagon.org>
2019-03-07 23:34:11 -0300

    work in progress

--- Assets/BlackjackScript.cs
+++ Assets/BlackjackScript.cs
@@ -21,6 +21,17 @@ public class BlackjackScript : MonoBehaviour
     }
 
     void win()
-    { 
+    {
+      if (player_score > 21) {
+        console.log("Print you lost")
+      } 
+      elsif player_score == 21
+        "Black Jack, you win!"
+      elsif player_score > bank_score
+        "You Win!"
+      elsif player_score < bank_score
+        "You Lose!"
+      else
+        "Push" 
     }
 }
